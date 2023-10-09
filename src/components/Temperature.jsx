8e86a24041687aef3c5e9de7912c47f52383747a
Ayush Srivastava <abz4375.ayushsrivastava@gmail.com>
2023-10-09 19:51:42 +0530

    enhancements

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -3,6 +3,16 @@ import "./Temperature.css";
 const Temperature = ({apidata}) => {
   return (
     <div className="temperature">
+      <p className="tem">
+      {
+        (apidata?.current_weather?.temperature)?("Temperature") : ""
+      }
+      </p>
+
+      {/* {
+        (apidata?.current_weather?.temperature)?(<br></br>):""
+      } */}
+
       {
         (apidata?.current_weather?.temperature)? 
         ( apidata?.current_weather?.temperature + " °C") 
